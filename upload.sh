286a116351d324604e82cb839dc1d30385073dc5
aviau <alexandre@alexandreviau.net>
2022-08-03 08:56:05 -0400

    twine: pypi-private -> pyro

--- upload.sh
+++ upload.sh
@@ -8,6 +8,6 @@ export AWS_PROFILE=production
 
 export TWINE_USERNAME=aws
 export TWINE_PASSWORD=$(aws codeartifact get-authorization-token --domain flared --domain-owner 409905535292 --query authorizationToken --output text)
-export TWINE_REPOSITORY_URL=$(aws codeartifact get-repository-endpoint --domain flared --domain-owner 409905535292 --repository pypi-private --format pypi --query repositoryEndpoint --output text)
+export TWINE_REPOSITORY_URL=$(aws codeartifact get-repository-endpoint --domain flared --domain-owner 409905535292 --repository pyro --format pypi --query repositoryEndpoint --output text)
 
-twine upload build/oso/languages/python/oso/dist/*linux_aarch64.whl
+twine upload build/oso/languages/python/oso/dist/*linux_aarch64.whl --verbose
